a0d94d005ded0a658b791a6d2d41c18fd37e2b67
zarkoc7 <zarko.rm7@gmail.com>
2023-05-23 23:20:06 +0200

    ubaceno fetchovanje podataka sa apija za quiz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,7 +1,60 @@
+import React,{useState, useEffect} from "react"
+import {nanoid} from "nanoid"
+
 function Quiz() {
-    
-        Return (<div>quiz</div>)
-        
+    const [quiz, setQuiz] = useState([])
+    const [restart, setRestart] = useState(false)
+
+    useEffect(() => {
+        fetch('https://opentdb.com/api.php?amount=5&type=multiple')
+        .then(res => res.json())
+        .then(data => {
+            let quizArray = []
+            let counter = 0
+            data.results.map(item => {
+                const incorrectAnswers = item.incorrect_answers.map(answer => {
+                    return {
+                        value : answer,
+                        isSelected : false,
+                        isCorrect : false,
+                        name: counter,
+                        id : nanoid()
+                    }
+                })
+
+                const correctAnswer = {
+                        value: item.correct_answer,
+                        isSelected : false,
+                        isCorrect: true,
+                        name: counter,
+                        id : nanoid()
+                    }
+
+                counter++
+
+                quizArray.push({
+                    question: item.question,
+                    answers: [...incorrectAnswers, correctAnswer]
+                })
+
+                // counter++
+
+                return quizArray
+            })
+
+            setQuiz(quizArray)
+        })
+    }, [restart])
+ 
+   
+
+
+   
+
+    return (
+        <div>Quiz</div>
+       
+    )
 }
 
 export default Quiz
