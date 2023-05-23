fcbad067c37818c01e5f4f28e34e268a4481cac2
zarkoc7 <zarko.rm7@gmail.com>
2023-05-23 23:04:55 +0200

    dodavanje routes za start and quiz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Start from "./components/Start";
+import Quiz from "./components/Quiz";
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Start />}></Route>
+
+          <Route path="/quiz" element={<Quiz />}></Route>
+        </Routes>
+      </Router>
+
     </div>
   );
 }
