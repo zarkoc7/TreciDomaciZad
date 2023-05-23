f0cf6ac7b197ad8308f3b1c329e7b86be17f4662
zarkoc7 <zarko.rm7@gmail.com>
2023-05-23 23:27:59 +0200

    dodati odgovori u question komponentu i sortiranje odgovora

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,36 @@
+import { nanoid } from "nanoid"
 import React from "react"
+import Answer from "./Answer"
 
 const he = require('he')
 
 function Question(props) {
 
-  
+    const allAnswers = props.answers.sort((a,b) => {
+        const answerA = a.value.toUpperCase()
+        const answerB = b.value.toUpperCase()
+        if(answerA < answerB)
+            return -1
+        if(answerA > answerB)
+            return 1
+        
+        return 0
+    })
 
+    const allAnswersElements = allAnswers.map(answer => {
+        return (
+            <Answer
+                key={nanoid()}
+                id = {answer.id} 
+                answer={answer.value}
+                name={answer.name}
+                handleChange={(event) => props.selectAnswer(event)}
+                isSelected = {answer.isSelected}
+                isCorrect= {answer.isCorrect}
+                isFinished={props.isFinished}
+            />
+        )
+    })
 
     return(
         <div className="question__container">
