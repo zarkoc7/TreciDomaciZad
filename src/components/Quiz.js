9772406009ed245c6924d20ba485941b5e2fbc84
zarkoc7 <zarko.rm7@gmail.com>
2023-05-23 23:33:44 +0200

    zavrsena quiz komponenta, dodati odg i pitanja

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,8 +1,11 @@
 import React,{useState, useEffect} from "react"
+import Question from "./Question"
 import {nanoid} from "nanoid"
 
 function Quiz() {
     const [quiz, setQuiz] = useState([])
+    const [score, setScore] = useState(0)
+    const [isFinished, setIsFinished] = useState(false)
     const [restart, setRestart] = useState(false)
 
     useEffect(() => {
@@ -46,14 +49,64 @@ function Quiz() {
         })
     }, [restart])
  
-   
+    function selectAnswer(event) {
+        const {id,name} = event.target
+        setQuiz(prevQuiz => {
+            return prevQuiz.map(item => {
+                return {
+                        ...item,
+                        answers : item.answers.map(answer => {
+                                return answer.name.toString() === name 
+                                    ? answer.id === id  
+                                        ? {...answer, isSelected: true}
+                                        : {...answer, isSelected: false}
+                                    : answer
+                            })
+                        }
+            })
+        })
+     }
 
+    function countScore() {
 
-   
+        if(isFinished)
+        {
+            setIsFinished(false)
+            setRestart(prevRestart => !prevRestart)
+        }
+        else
+        {
+            let result = 0
+            quiz.map(item => {
+                return item.answers.map(answer => {
+                    return answer.isSelected && answer.isCorrect
+                        ? result++
+                        : 0
+                })
+            })
+            setScore(result)
+            setIsFinished(true)
+        }
+    }
+
+    const questionsElements = quiz.map(item => {
+        return <Question 
+                    key={nanoid()} 
+                    question={item.question} 
+                    answers={item.answers}
+                    selectAnswer={selectAnswer}
+                    isFinished={isFinished}
+                />
+    })
 
     return (
-        <div>Quiz</div>
-       
+        <div className="quiz">
+            {questionsElements}
+            <div className="quiz__footer">
+                {isFinished && <h4 className="quiz__results">You scored {score}/5 correct answers</h4> }
+                {questionsElements.length > 0 && <button onClick={countScore}>{isFinished ? "Play Again" : "Check answers"}</button>}
+            </div>
+        </div>
     )
 }
 
